3489192ef14c97c83cf15c7bf53e48efbdd57938
Mridul Singh <mridulsingh@codeaurora.org>
2017-03-01 11:37:34 -0800

    Camera: Add NULL check on mDevice to avoid crash.
    NULL check on mDevice before calling cancelAutoFocus.
    Also adding the same check in disconnect.
    
    CRs-Fixed: 2000357 1082919
    Change-Id: If57e12913bebfca6ab9b871a0bdcc24d663e7107
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- services/camera/libcameraservice/api1/Camera2Client.cpp
+++ services/camera/libcameraservice/api1/Camera2Client.cpp
@@ -452,9 +452,10 @@ binder::Status Camera2Client::disconnect() {
 
     ALOGV("Camera %d: Disconnecting device", mCameraId);
 
-    mDevice->disconnect();
-
-    mDevice.clear();
+    if (mDevice != nullptr) {
+        mDevice->disconnect();
+        mDevice.clear();
+    }
 
     CameraService::Client::disconnect();
 
@@ -1371,7 +1372,7 @@ status_t Camera2Client::cancelAutoFocus() {
     ALOGV("%s: Camera %d", __FUNCTION__, mCameraId);
     status_t res;
     if ( (res = checkPid(__FUNCTION__) ) != OK) return res;
-
+    if (mDevice == 0) return NO_INIT;
     int triggerId;
     {
         SharedParameters::Lock l(mParameters);
